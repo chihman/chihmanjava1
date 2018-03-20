47a9c9bb7a0a2f03db344a5b51a36be8d3a55a29
chihman <chihmanD2017gmail.com>
2018-03-20 12:23:08 +0600

    lab1d

--- src/lab12/task5164.java
+++ src/lab12/task5164.java
@@ -18,6 +18,18 @@ public class task5164 {
       lab01.task7472("шаг "," шаг " );
 
       lab01.task7271("5","7");
+      lab01.task7271("-1","1000");
+
+      lab01.task2632("50","50", "40", "yellow");
+        lab01.task2632("400","300", "200", "green");
+        lab01.task2632("254","356", "100", "#cc6600");
+
+        lab01.task4343("test","pass","localhost","5432","my_db");
+        lab01.task4343("admin","7e8efd48d69c","production-node1","5432","bank");
+        lab01.task4343("akryukov","123","localhost","dev-node1","bank");
+
+      lab01.task7474("1","2","3");
+      lab01.task7474("-3","0","2");
     }
 
 
--- src/labs/lab01.java
+++ src/labs/lab01.java
@@ -10,12 +10,21 @@ public class lab01 {
         System.out.println(a+" нас не остановят" );
     }
     public static void task7472(String a,String b ) {
-        System.out.println(b + a);
+        System.out.println(a + b);
 
     }
     public static void task7271(String a,String b ) {
-        System.out.println("INSERT INTO points (x, y) VALUES" + a+b);
-
-
+        System.out.println("INSERT INTO points (x, y) \n VALUES" +" "+"('"+a+"'"+" ,"+"'"+b+"')");
+           }
+    public static void task2632(String a,String b,String c,String d ) {
+        System.out.println("<circle cx="+a+" cy="+b+" r="+c+" fill="+d+"/>");
     }
+    public static void task4343(String a, String b, String c, String d, String e);
+    {
+    System.out.println("User ID="+a+";Password="+b+"; Host="+c+";Port="+d+";Database="+e+";");}
+
+    public static void task7474(String a, String b, String c);
+    {
+        System.out.println("Квадратное уравнение "+a+"*x*x + "+b+"*x + "+c+" = 0.\n" +
+                "Его дискриминант вычисляется по формуле: d = "+b+"*"+b+" - 4*"+a+"*"+c+"");}
 }
