0df46d46929cc1a369a985c232d058e8b0758226
Chihman <chihmanD2017@gmail.com>
2018-04-03 19:40:32 +0600

    14

---
+++ src/lab14/task8403/Point.java
@@ -0,0 +1,15 @@
+package lab14.task8403;
+
+/**
+ * @author akryukov
+ *         13.07.2017
+ */
+public class Point {
+    public int x;
+    public int y;
+    public double distanceTo(int px, int py){
+        int dx = px - this.x;
+        int dy = py - y;
+        return Math.sqrt(dx*dx + dy*dy);
+    }
+}
---
+++ src/lab14/task9914/Interval.java
@@ -0,0 +1,24 @@
+package lab14.task9914;
+
+
+
+/**
+ * Created by pro-28 on 03.04.2018.
+ */
+public class Interval {
+
+    public int a;
+    public int b;
+public boolean intersects (Interval r){
+
+
+}
+    public double distanceTo(lab14.task3185.Point p) {
+
+        if (a == b) {
+            throw new IllegalArgumentException("Пересекаются");
+        }
+
+
+
+}}
