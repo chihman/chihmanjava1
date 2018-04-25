ba7c0e290eb75fdaa373e06e56d763ea2ce5aab2
chihman <chihmanD2017gmail.com>
2018-04-25 09:50:24 +0600

    дом 25-04

--- src/J2/NasledovanieMain.java
+++ src/J2/NasledovanieMain.java
@@ -5,7 +5,8 @@ import j22.NasledovanieCamri50;
 public class NasledovanieMain {
     public static void main(String[] args) {
         NasledovanieCamri50 camry50 = new NasledovanieCamri50();
-        camry50.drive();
         camry50.light();
+        camry50.drive();
+
     }
 }
