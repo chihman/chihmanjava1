9a38d69d374e52b5e1ddf0f445a23b5178b2e85d
chihman <chihmanD2017gmail.com>
2018-04-28 11:24:37 +0600

    dz

--- src/lab14/Task1953.java
+++ src/lab14/Task1953.java
@@ -16,22 +16,22 @@ public class Task1953 {
 
     private static void Step1(){
         Circle c = new Circle(100, 200, 50);
-        System.out.println(c);
+        System.out.println("<circle cx=" + c + " cy=" +c+ " r=" + c  + "/>");
     }
 
     private static void Step2(){
         Circle c = new Circle(40, 50, 13);
         System.out.printf("%.4f\n", c.area());
     }
-//
-//    private static void Step3(){
-//        Circle c = new Circle(100, 120, 17);
-//        Interval px = c.getProjectionX();
-//        Interval py = c.getProjectionY();
-//        System.out.println(px.print());
-//        System.out.println(py.print());
-//    }
-//
+
+        private static void Step3(){
+        Circle c = new Circle(100, 120, 17);
+        Interval px = c.getProjectionX();
+        Interval py = c.getProjectionY();
+        System.out.println(px.print());
+        System.out.println(py.print());
+    }
+
 //    private static void Step4(){
 //        Circle c = new Circle(100, 120, 31);
 //        System.out.println(c.getProjectionX().print());
--- src/lab14/task1953/Interval.java
+++ src/lab14/task1953/Interval.java
@@ -4,4 +4,24 @@ package lab14.task1953;
  * Created by pro-28 on 25.04.2018.
  */
 public class Interval {
+    private int ProjectionX;
+    private int ProjectionY;
+
+
+    public int getProjectionX() {
+        return ProjectionX;
+    }
+
+    public void setProjectionX(int projectionX) {
+        ProjectionX = projectionX;
+    }
+
+    public int getProjectionY() {
+        return ProjectionY;
+    }
+
+    public void setProjectionY(int projectionY) {
+        ProjectionY = projectionY;
+    }
 }
+
